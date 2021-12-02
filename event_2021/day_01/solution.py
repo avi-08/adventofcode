b4cf0bd4f9e8586a45ba4a44c33bb9dc6f637da6
windstorm <avi.1199863@gmail.com>
2021-12-02 16:25:34 +0530

    event_2021 day_02

---
+++ event_2021/day_01/solution.py
@@ -0,0 +1,50 @@
+import os
+
+
+def read_input(file_name='input.txt'):
+    file_path = os.path.join(os.path.curdir, file_name)
+
+    with open(file_path, 'r') as f:
+        depths = f.readlines()
+
+    depths = [int(depth) for depth in depths]
+    return depths
+
+
+def find_increased_depths(depths):
+    increased = 0
+    pre = depths[0]
+
+    for depth in depths[1:]:
+        if depth > pre:
+            increased += 1
+        pre = depth
+    
+    return increased
+
+
+def find_increased_window_depths(depths, window_size=1):
+    window_end = len(depths) - window_size
+    pre = sum(depths[:window_size])
+    increased = 0
+    for index in range(0, window_end):
+        curr = pre - depths[index] + depths[index+window_size]
+        if curr > pre:
+            increased += 1
+        pre = curr
+    
+    return increased
+
+
+depths = read_input()
+total_depths = len(depths)
+print(f"Input Depths: {total_depths}")
+
+# Part one
+increased = find_increased_depths(depths)
+print(f"Depth increased {increased} times out of {total_depths}")
+
+# Part two
+window = 3
+increased_window = find_increased_window_depths(depths, window)
+print(f"Depth increased within window of {window} readings: {increased_window} times out of {total_depths-window+1}")
---
+++ event_2021/day_02/solution.py
@@ -0,0 +1,60 @@
+import os
+
+
+class DIRECTIONS:
+    UP = "up"
+    DOWN = "down"
+    FORWARD = "forward"
+
+
+def read_input(file_name='input.txt'):
+    file_path = os.path.join(os.path.curdir, file_name)
+
+    with open(file_path, 'r') as f:
+        moves = f.readlines()
+
+    moves = [(move.split(" ")[0], int(move.split(" ")[1])) for move in moves]
+    return moves
+
+
+def calculate_position(moves):
+    x_loc, y_loc = 0, 0
+    for move in moves:
+        if move[0] == DIRECTIONS.FORWARD:
+            x_loc += move[1]
+        elif move[0] == DIRECTIONS.DOWN:
+            y_loc += move[1]
+        else:
+            y_loc -= move[1]
+    return x_loc, y_loc
+
+
+def calculate_position_w_aim(moves):
+    x_loc, y_loc, aim = 0, 0, 0
+    for move in moves:
+        if move[0] == DIRECTIONS.FORWARD:
+            x_loc += move[1]
+            y_loc += (aim * move[1])
+        elif move[0] == DIRECTIONS.DOWN:
+            aim += move[1]
+        else:
+            aim -= move[1]
+    return x_loc, y_loc, aim
+
+
+moves = read_input()
+print(f"Total moves: {len(moves)}")
+
+# Part one
+pos, depth = calculate_position(moves)
+print(f"Moved long X-axis: {pos} units")
+print(f"Moved along Y-axis: {depth} units")
+print(f"Co-ordinate product: {pos * depth}")
+
+print("\n<=============>\n")
+# Part two
+pos, depth, aim = calculate_position_w_aim(moves)
+print(f"Moved long X-axis: {pos} units")
+print(f"Moved along Y-axis: {depth} units")
+print(f"Current aim setting: {aim} ")
+print(f"Co-ordinate product: {pos * depth}")
